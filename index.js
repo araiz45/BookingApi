3ed2e2495ee20b9a7d789d5248391a5ded2c157b
araiz45 <araizzahid45@gmail.com>
2023-11-29 01:37:02 +0500

    deploy heroku3

--- index.js
+++ index.js
@@ -18,8 +18,19 @@ app.use(
   cors({
     credentials: true,
     origin: process.env.ORIGIN,
+    optionSuccessStatus: 200,
   })
 );
+app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+});
 // user routes
 app.use("/api/user", userRoute);
 
